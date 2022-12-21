b37941fbea61f5e2be9f0e4b2b15538fb2917bae
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-21 14:00:24 +0700

    Implement game function
    Complete the basic JavaScript part of this Rock Paper Scissors
    project.

--- script.js
+++ script.js
@@ -61,3 +61,35 @@ function displayWinner(userChoice, computerChoice) {
       return "Draw! Both choose Scissors";
   }
 }
+
+function game() {
+  // set up user and com point
+  let userScore = 0;
+  let comScore = 0;
+
+  // loop until user or com score 3
+  while (userScore < 3 && comScore < 3) {
+    // input choices and get result
+    let userChoice = getUserChoice();
+    let comChoice = getComputerChoice();
+    let result = displayWinner(userChoice, comChoice);
+
+    // calculate score
+    if (result.includes("Win"))
+      userScore++;
+    if (result.includes("Lose"))
+      comScore++;
+    
+    // display choice made
+    alert(`User chooses ${userChoice}, Computer choose ${comChoice}`);
+
+    // display result and score
+    alert(`${result}\nCurrent score: user/${userScore} - com/${comScore}`);
+  }
+
+  // display final winners
+  let winner = (userScore > comScore) ? "user" : "computer";
+  alert(`The final winner is ${winner}`);
+}
+
+game();
