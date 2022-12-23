eeed58c543ece5f48ec0ebdbf1ac5d1e67c47fcf
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-23 22:55:07 +0700

    Implement UI for play 1 round

--- script.js
+++ script.js
@@ -1,95 +1,51 @@
 function getComputerChoice() {
-  // generate random number within 0, 1, 2
-  let choice = Math.floor(Math.random()*3);
+  return Math.floor(Math.random()*3);
+}
 
-  // if 0 -> rock, 1 -> paper, else -> scissors
-  if (choice == 0)
-    return "rock";
-  else if (choice == 1)
-    return "paper";
+function compareChoice(playerChoice, comChoice) {
+  if ((playerChoice+1)%3 == comChoice)
+    return -1;
+  else if (playerChoice == comChoice)
+    return 0;
   else
-    return "scissors";
+    return 1;
 }
 
-function getUserChoice() {
-  let input;
+function displayResult(playerChoice, comChoice) {
+  let resultFlag = compareChoice(playerChoice, comChoice);
 
-  // loop
-  while(true) {
-    // get input from prompt
-    input = prompt("Enter 'rock', 'paper' or 'scissors':");
-    
-    // if input is null, execute the loop again
-    if (input == null) continue;
-
-    // change input to lower case
-    input = input.toLowerCase();
-
-    // if input is rock, paper or scissors, break the loop
-    if (input == "rock" || input == "paper" || input == "scissors")
-      break;
-  }
-
-  return input;
+  const result = document.querySelector('p#result');
+  if (resultFlag == 0)
+    result.textContent = "Draw. You both did well.";
+  else if (resultFlag == 1)
+    result.textContent = "You win. How lucky!";
+  else
+    result.textContent = "You loose. Maybe next time!";
 }
 
-function displayWinner(userChoice, computerChoice) {
-  if (userChoice == "rock") {
-    if (computerChoice == "paper")
-      return "You Lose! Paper beats Rock";
-    else if (computerChoice == "scissors")
-      return "You Win! Rock beats Scissors"
-    else
-      return "Draw! Both choose Rock"
-  }
-
-  if (userChoice == "paper") {
-    if (computerChoice == "scissors")
-      return "You Lose! Scissors beat Paper";
-    else if (computerChoice == "rock")
-      return "You Win! Paper beats Rock";
-    else
-      return "Draw! Both choose Paper"
-  }
-
-  if (userChoice == "scissors") {
-    if (computerChoice == "rock")
-      return "You Lose! Rock beats Scissors";
-    else if (computerChoice == "paper")
-      return "You Win! Scissors beat Paper";
-    else
-      return "Draw! Both choose Scissors";
-  }
+function choiceToString(choice) {
+  if (choice == 0)
+    return "Rock";
+  else if (choice == 1)
+    return "Paper";
+  else
+    return "Scissors";
 }
 
-function game() {
-  // set up user and com point
-  let userScore = 0;
-  let comScore = 0;
-
-  // loop until user or com score 3
-  while (userScore < 3 && comScore < 3) {
-    // input choices and get result
-    let userChoice = getUserChoice();
-    let comChoice = getComputerChoice();
-    let result = displayWinner(userChoice, comChoice);
-
-    // calculate score
-    if (result.includes("Win"))
-      userScore++;
-    if (result.includes("Lose"))
-      comScore++;
-    
-    // display choice made
-    alert(`User chooses ${userChoice}, Computer choose ${comChoice}`);
-
-    // display result and score
-    alert(`${result}\nCurrent score: user/${userScore} - com/${comScore}`);
-  }
-
-  // display final winners
-  let winner = (userScore > comScore) ? "user" : "computer";
-  alert(`The final winner is ${winner}`);
+function displayChoice(playerChoice, comChoice) {
+  const choices = document.querySelector('p#choices');
+  choices.textContent = `Player chose ${choiceToString(playerChoice)}. `;
+  choices.textContent += `Computer chose ${choiceToString(comChoice)}.`;
 }
 
-game();
+function playRound(e) {
+  const playerChoice = parseInt(this.value);
+  const comChoice = getComputerChoice();
+
+  displayChoice(playerChoice, comChoice);
+  displayResult(playerChoice, comChoice);
+}
+
+// set up elements and event listeners
+const buttons = document.querySelectorAll('button.choice');
+buttons.forEach(btn => btn.addEventListener('click', playRound));
